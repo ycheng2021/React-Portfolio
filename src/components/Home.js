1997acdabda3d95673217ff32f07e60ac6930530
Yang Cheng <annacheng09@hotmail.com>
2022-05-16 21:07:14 -0700

    changing the layout

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import '../styles/home.css';
 
-const Home = () => {
+export default function About() {
     return (
-        <div>
-            <h1>Hello World</h1>
-        </div>
-    )
-}
-
-export default Home;
+      <div>
+        <h1>Home Page</h1>
+        <p>Introduction to myself</p>
+        <p>Skills</p>
+        
+      </div>
+    );
+  }
