8b8615620128c1c436fe9014aec4a9b16ec1308e
Yang Cheng <annacheng09@hotmail.com>
2022-05-25 13:47:54 -0700

    change the design layout

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import NavTabs from "./components/NavTabs"
 import Home from "./components/Home";
 import Footer from "./components/Footer"
+import About from "./pages/About"
+import Portfolio from "./pages/Portfolio"
 
 function App() {
     return (
         <Router>
-            <NavTabs />
             <Routes>
                 <Route 
                     path="/"
                     element={<Home/>}
+                />
+                <Route 
+                    path="/about"
+                    element={<About/>}
+                />
+                <Route 
+                    path="/portfolio"
+                    element={<Portfolio/>}
                 />
                 <Route 
                     path="*"
