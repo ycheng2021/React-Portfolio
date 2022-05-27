949c862b1f9b7ba8bd805f2f39f891486fa46371
Yang Cheng <annacheng09@hotmail.com>
2022-05-26 19:06:39 -0700

    deploy

--- src/App.js
+++ src/App.js
@@ -5,34 +5,37 @@ import Footer from "./components/Footer";
 import About from "./pages/About";
 import Portfolio from "./pages/Portfolio";
 import SingleProject from "./pages/SingleProject";
+import "./App.css";
 
 function App() {
   const projects = [
-    { name: "home-improvement"},
-    { name: "el-caro-modelo"},
-    { name: "the-grand-line-anime"},
-    { name: "ultimate-tech-blog"},
-    { name: "weather-dashboard"},
-    { name: "code-quiz"},
+    { name: "home-improvement" },
+    { name: "el-caro-modelo" },
+    { name: "the-grand-line-anime" },
+    { name: "ultimate-tech-blog" },
+    { name: "weather-dashboard" },
+    { name: "code-quiz" },
   ];
 
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/portfolio" element={<Portfolio />} />
-        {projects.map((project) => (
-          <Route
-            key={project.name}
-            path={`portfolio/:${project.name}`}
-            element={<SingleProject />}
-          />
-        ))}
-        <Route path="*" element={<h1>Wrong Page!!</h1>} />
-      </Routes>
-      <Footer />
-    </Router>
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/portfolio" element={<Portfolio />} />
+          {projects.map((project) => (
+            <Route
+              key={project.name}
+              path={`portfolio/:${project.name}`}
+              element={<SingleProject />}
+            />
+          ))}
+          <Route path="*" element={<h1>Wrong Page!!</h1>} />
+        </Routes>
+        <Footer />
+      </Router>
+    </div>
   );
 }
 
