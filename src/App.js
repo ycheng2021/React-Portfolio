ded2c4fffbaf42436d2403d1ed8418f56ed1d0df
Yang Cheng <annacheng09@hotmail.com>
2022-05-27 01:05:28 -0700

    add switch in router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Switch } from "react-router";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import Footer from "./components/Footer";
@@ -19,19 +20,19 @@ function App() {
 
   return (
     <Router>
-      <Routes>
-        <Route path="/react-porfolio" element={<Home />} />
-        <Route path="react-portfolio/about" element={<About />} />
-        <Route path="react-portfolio/portfolio" element={<Portfolio />} />
+      <Switch>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/portfolio" element={<Portfolio />} />
         {projects.map((project) => (
           <Route
             key={project.name}
-            path={`react-portfolio/portfolio/:${project.name}`}
+            path={`/portfolio/:${project.name}`}
             element={<SingleProject />}
           />
         ))}
         <Route path="*" element={<h1>Wrong Page!!</h1>} />
-      </Routes>
+      </Switch>
       <Footer />
     </Router>
   );
